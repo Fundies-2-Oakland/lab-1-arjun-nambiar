498934dcb01cf739d87e7aaa3325c825105b110d
Arjun Nambiar <atn92656@gmail.com>
2025-01-10 16:21:29 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+//Q 1 & 2
+
+public class Main {
+
+    public static class Vector3D{
+        double x;
+        double y;
+        double z;
+
+        public Vector3D(double x, double y, double z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Double getX(){
+            return x;
+        }
+
+        public Double getY(){
+            return y;
+        }
+
+        public Double getZ(){
+            return z;
+        }
+
+
+
+    }
+
+}
